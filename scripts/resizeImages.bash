124b2c4c3b4c28a1970d397f9ff96e4d98af1752
Hanno Rein <hanno@hanno-rein.de>
2020-06-21 19:14:56 -0400

    resize

---
+++ scripts/resizeImages.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+rm -v ../data_images/*.iPhone*
+rm -v ../data_images/*.iPad*
+rm -v ../data_images/*.web*
+
+
+for f in ../data_images/*.png ../data_images/*.jpg ../data_images/*.jpeg
+do
+	filename=${f%.*}
+	echo $filename
+	convert -resize x260 -resize '960x<' -resize 50% -gravity center -crop 480x130+0+0 +repage -quality 90 "$f" "$filename.iPhone.png"
+	convert -resize x260 -resize '2048x<' -resize 50% -gravity center -crop 1024x130+0+0 +repage -quality 90 "$f" "$filename.iPad.png"
+	convert -resize x400 -resize '1200x<' -resize 50% -gravity center -crop 600x200+0+0 +repage -quality 90 "$f" "$filename.web.png"
+	convert -resize 800x600 -quality 90 "$f" "$filename.weblarger.png"
+done
+
