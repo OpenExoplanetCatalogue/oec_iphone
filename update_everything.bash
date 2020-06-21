638979007230ff816f30041058400a6479d970f2
Hanno Rein <hanno@hanno-rein.de>
2020-06-21 19:28:21 -0400

    submodules removed

--- update_everything.bash
+++ update_everything.bash
@@ -1,10 +1,8 @@
 #!/bin/bash
 alias python=python2.7
 
-pushd open_exoplanet_catalogue
-git pull
-git checkout master
-popd
+rm -r open_exoplanet_catalogue
+git clone --depth=1 git@github.com:OpenExoplanetCatalogue/open_exoplanet_catalogue.git
 
 pushd scripts
 python create_data_iphone_11.python
@@ -12,14 +10,10 @@ popd
 
 ## Images
 
-pushd oec_images
-git pull
-git checkout master
-popd
-
-rm -r data_images
+rm -r oec_images
+git clone --depth=1 git@github.com:OpenExoplanetCatalogue/oec_images.git
 cp -r oec_images/images data_images
 
 pushd scripts
-python resizeImages.bash
+./resizeImages.bash
 popd
