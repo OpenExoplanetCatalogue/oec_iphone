6da6d8e32a37408d36eb089c706ff428438774d3
Hanno Rein <hanno@hanno-rein.de>
2020-06-21 19:44:07 -0400

    more

--- scripts/resizeImages.bash
+++ scripts/resizeImages.bash
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-for f in ../data_images/*.png ../data_images/*.jpg ../data_images/*.jpeg
+mkdir ../data_images
+cp ../oec_images/images/* ../data_images/
+
+for f in ../oec_images/images/*.png ../oec_images/images/*.jpg ../oec_images/images/*.jpeg
 do
-	filename=${f%.*}
-	echo $filename
-	convert -resize x260 -resize '960x<' -resize 50% -gravity center -crop 480x130+0+0 +repage -quality 90 "$f" "$filename.iPhone.png"
-	convert -resize x260 -resize '2048x<' -resize 50% -gravity center -crop 1024x130+0+0 +repage -quality 90 "$f" "$filename.iPad.png"
-	convert -resize x400 -resize '1200x<' -resize 50% -gravity center -crop 600x200+0+0 +repage -quality 90 "$f" "$filename.web.png"
-	convert -resize 800x600 -quality 90 "$f" "$filename.weblarger.png"
+    fb=$(basename "$f")
+	echo $fb
+	filename=${fb%.*}
+	convert -resize x260 -resize '960x<' -resize 50% -gravity center -crop 480x130+0+0 +repage -quality 90 "../oec_images/images/$fb" "../data_images/$filename.iPhone.png"
+	convert -resize x260 -resize '2048x<' -resize 50% -gravity center -crop 1024x130+0+0 +repage -quality 90 "../oec_images/images/$fb" "../data_images/$filename.iPad.png"
+	convert -resize x400 -resize '1200x<' -resize 50% -gravity center -crop 600x200+0+0 +repage -quality 90 "../oec_images/images/$fb" "../data_images/$filename.web.png"
+	convert -resize 800x600 -quality 90 "../oec_images/images/$fb" "../data_images/$filename.weblarger.png"
 done
 
--- update_everything.bash
+++ update_everything.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
 alias python=python2.7
 
-rm -r open_exoplanet_catalogue
 git clone --depth=1 git@github.com:OpenExoplanetCatalogue/open_exoplanet_catalogue.git
+pushd open_exoplanet_catalogue
+git pull
+popd
 
 pushd scripts
 python create_data_iphone_11.python
@@ -10,9 +12,10 @@ popd
 
 ## Images
 
-rm -r oec_images
 git clone --depth=1 git@github.com:OpenExoplanetCatalogue/oec_images.git
-cp -r oec_images/images data_images
+pushd oec_images
+git pull
+popd
 
 pushd scripts
 ./resizeImages.bash
