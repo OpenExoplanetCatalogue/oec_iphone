e23e213196dcc13f3266c5926d235c415a7cd92c
Hanno Rein <hanno@hanno-rein.de>
2021-01-02 00:25:34 +0100

    size change

--- scripts/resizeImages.bash
+++ scripts/resizeImages.bash
@@ -9,7 +9,7 @@ do
 	echo $fb
 	filename=${fb%.*}
 	convert -resize x260 -resize '960x<' -resize 50% -gravity center -crop 480x130+0+0 +repage -quality 90 "../oec_images/images/$fb" "../data_images/$filename.iPhone.png"
-	convert -resize x260 -resize '2048x<' -resize 50% -gravity center -crop 1024x130+0+0 +repage -quality 90 "../oec_images/images/$fb" "../data_images/$filename.iPad.png"
+	convert -resize x260 -resize '2048x<' -resize 50% -gravity center -crop 1024x260+0+0 +repage -quality 90 "../oec_images/images/$fb" "../data_images/$filename.iPad.png"
 	convert -resize x400 -resize '1200x<' -resize 50% -gravity center -crop 600x200+0+0 +repage -quality 90 "../oec_images/images/$fb" "../data_images/$filename.web.png"
 	convert -resize 800x600 -quality 90 "../oec_images/images/$fb" "../data_images/$filename.weblarger.png"
 done
