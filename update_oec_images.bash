629095ac3779d4946a4e4d50dd414086b8b7f731
Hanno Rein <hanno@hanno-rein.de>
2023-02-20 20:04:57 +0000

    venv

--- update_oec_images.bash
+++ update_oec_images.bash
@@ -1,6 +1,4 @@
 #!/bin/bash
-alias python=python2.7
-
 pushd scripts
 ./resizeImages.bash
 popd
--- update_open_exoplanet_catalogue.bash
+++ update_open_exoplanet_catalogue.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-bash /home/rein/oec_web/venv/bin/activate
+source /home/rein/oec_web/venv/bin/activate
 
 pushd scripts
 python create_data_iphone_11.python
